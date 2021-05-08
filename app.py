93ca9466945c4c6f7d8e429e5d050909fac1113a
nbgh46 <nbgh46@naver.com>
2021-05-08 15:47:51 +0900

    api 작업 완료

--- app.py
+++ app.py
@@ -1,13 +1,71 @@
-from flask import Flask, render_template
-
+import requests
+from flask import Flask, render_template, request, jsonify
+from bs4 import BeautifulSoup
 # 플라스크 웹 서버 생성하기
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+# mongodb 추가
+client = MongoClient('localhost', 27017)
+db = client.get_database('sparta')
 
 
 # API 추가
 @app.route('/', methods=['GET'])  # 데코레이터 문법
 def index():
-    return render_template('index.html', test ='테스트')
+    memos = list(db.articles.find({}, {'id': False}))
+    return render_template('index.html', test='테스트')
+
+
+# 아티클 추가 api
+@app.route('/memo', methods=['POST'])
+def save_memo():
+    form = request.form
+    url_receive = form['url_give']
+    comment_receive = form['comment_give']
+    headers = headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.get(
+        url_receive,
+        headers=headers
+
+    )
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')
+    url = soup.select_one('meta[property="og:url"]')
+    image = soup.select_one('meta[property="og:image"]')
+    description = soup.select_one('meta[property="og:description"]')
+
+    print(title['content'])
+    print(url['content'])
+    print(image['content'])
+    print(description['content'])
+
+    print(url_receive, comment_receive)
+    document = {
+        'title': title['content'],
+        'url': url['content'],
+        'image': image['content'],
+        'description': description['content'],
+        'comment': comment_receive,
+    }
+    db.articles.insert_one(document)
+
+    return jsonify(
+        {'result': 'success', 'msg': '저장했습니다.'}
+    )
+
+
+@app.route('/memo', methods=['GET'])
+def list_memo():
+    memos = list(db.articles.find({}, {'_id': 0}))
+    result = {
+        'result': 'success',
+        'articles': memos,
+    }
+    return jsonify(result)
 
 
 # app.py 파일을 직접 실행시킬 떄 동작시킴
